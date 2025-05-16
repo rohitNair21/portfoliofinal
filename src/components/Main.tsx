266627102cd5590cd266a1fb3ca3311617236f18
rohitNair21 <nairrohit003@gmail.com>
2025-05-16 09:30:39 -0600

    Initial updates of my own content

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { TypeAnimation } from 'react-type-animation';
+import avatar from '../assets/images/avatar.png';
 import '../assets/styles/Main.scss';
 
 function Main() {
@@ -9,20 +11,31 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img className="profile-avatar" src={avatar} alt="Avatar"/>
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/rohitNair21" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/rohit-n-345957192/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
-
+          <p>
+              <TypeAnimation
+                sequence={[
+                  'Hello, I am', 2500,
+                  'Greetings, I go by', 2500,
+                  "Hey, it's", 2500
+                ]}
+                wrapper="span"
+                speed={50}
+                style={{ fontSize: '1em', display: 'inline-block', fontWeight: 'bold' }}
+                repeat={Infinity}
+              />
+          </p>
+          <h1>Rohit Nair</h1>
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
-          </div>
+            <a href="https://github.com/rohitNair21" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/rohit-n-345957192/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            </div>
         </div>
       </div>
     </div>
